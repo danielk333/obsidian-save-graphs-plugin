a2e78b37268372fb1a006ae1075155bc23495f34
danielk <daniel.kastinen@irf.se>
2023-05-11 23:02:09 +0200

    first verison

--- main.ts
+++ main.ts
@@ -1,81 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App, SuggestModal, WorkspaceLeaf,
+	Modal, Plugin, Notice,
+	PluginSettingTab, Setting
+	} from 'obsidian';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as glob from 'glob';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface SaveGraphsSettings {
+	dataStore: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SaveGraphsSettings = {
+	dataStore: '.obsidian/graphs'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const GRAPH_DATA_PATH = '.obsidian/graph.json';
 
+export default class SaveGraphs extends Plugin {
+	settings: SaveGraphsSettings;
+	dataStorePath: string;
+	graphDataPath: string;
+
+	
 	async onload() {
 		await this.loadSettings();
+		
+		const root = (this.app.vault.adapter as any).basePath;
+		this.dataStorePath = path.join(root, this.settings.dataStore);
+		this.graphDataPath = path.join(root, GRAPH_DATA_PATH);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (!fs.existsSync(this.dataStorePath)){
+			fs.mkdirSync(this.dataStorePath);
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'load-graph-data',
+			name: 'Load a saved graph [NOT WORKING]',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new LoadGraph(this.app, this.dataStorePath, this.graphDataPath).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'save-graph-data',
+			name: 'Save current graph',
+			callback: () => {
+				new SaveGraph(this.app, this.dataStorePath, this.graphDataPath).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SaveGraphsSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,14 +66,86 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+
+interface Graph {
+	name: string;
+}
+
+class LoadGraph extends SuggestModal<Graph> {
+	dataStore: string;
+	graphPath: string;
+	Graphs: Graph[];
+
+	constructor(app: App, dataStore: string, graphPath: string) {
 		super(app);
+		this.dataStore = dataStore;
+		this.graphPath = graphPath;
+		this.Graphs = [];
+		var files = glob.globSync(path.join(this.dataStore, '*.json'));
+		files.forEach( (file: string) => {
+			this.Graphs.push({name: path.parse(file).name})
+		});
 	}
+
+	getSuggestions(query: string): Graph[] {
+		return this.Graphs.filter((graph) =>
+			graph.name.toLowerCase().includes(query.toLowerCase())
+		);
+	}
+
+	renderSuggestion(graph: Graph, el: HTMLElement) {
+		el.createEl("div", { text: graph.name });
+	}
+
+	onChooseSuggestion(graph: Graph, evt: MouseEvent | KeyboardEvent) {
+		var target = path.join(this.dataStore, graph.name + '.json');
+		fs.copyFile(target, this.graphPath, (err) => {
+			if (err) throw err;
+		});
+
+		// FIGURE OUT HOW TO FORCE RELOAD OF THE FILE AND THIS JUST WORKS
+	}
+}
+
+class SaveGraph extends Modal {
+	name: string;
+	dataStore: string;
+	graphPath: string;
+
+	constructor(app: App, dataStore: string, graphPath: string) {
+		super(app);
+		this.dataStore = dataStore;
+		this.graphPath = graphPath;
+	}
+
+	onSubmit(name: string) {
+		var target = path.join(this.dataStore, name + '.json');
+		fs.copyFile(this.graphPath, target, (err) => {
+			if (err) throw err;
+		});
+	};
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+
+		contentEl.createEl("h3", { text: "Save graph" });
+
+		new Setting(contentEl)
+		  .setName("Name")
+		  .addText((text) =>
+			text.onChange((value) => {
+			  this.name = value
+			}));
+	
+		new Setting(contentEl)
+		  .addButton((btn) =>
+			btn
+			  .setButtonText("Save")
+			  .setCta()
+			  .onClick(() => {
+				this.close();
+				this.onSubmit(this.name);
+			  }));
 	}
 
 	onClose() {
@@ -107,10 +154,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SaveGraphsSettingTab extends PluginSettingTab {
+	plugin: SaveGraphs;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SaveGraphs) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +167,17 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Save graphs plugin settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Graph storage path')
+			.setDesc('Where to store the json files (relative to vault root)')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('.obsidian/graphs')
+				.setValue(this.plugin.settings.dataStore)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+					this.plugin.settings.dataStore = value;
+					await this.plugin.saveSettings(); 
 				}));
 	}
 }
